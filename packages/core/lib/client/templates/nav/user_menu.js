372615fdd2782426ac84040cd72386435c6dc998
Thomas Jeanneau <thomasj230@gmail.com>
2016-03-17 01:10:59 +0100

    add colors + settings

---
+++ packages/core/lib/client/templates/nav/user_menu.js
@@ -0,0 +1,36 @@
+/**
+ * Created by thomas on 17/03/16.
+ */
+
+Template.ns_user_menu.helpers({
+    user: function () {
+        return Meteor.user();
+    },
+    menuItems: function () {
+        var viewableItems = _.reject(Telescope.menuItems.get("userMenu"), function (item) {
+            return (item.adminOnly && !Users.is.admin(Meteor.user()));
+        });
+
+        // viewableItems = viewableItems.map(function (item) {
+        //   item.parentId = "userMenuRoot";
+        //   return item;
+        // });
+
+        // viewableItems.push({
+        //   id: "userMenuRoot",
+        //   template: "user_menu_label"
+        // });
+        // console.log(viewableItems);
+
+        return viewableItems;
+    },
+    menuType: function () {
+        if (this.zone === "mobileNav") {
+            return 'collapsible';
+        } else if (Settings.get('navLayout', 'top-nav') === 'top-nav') {
+            return 'dropdown';
+        } else {
+            return 'collapsible';
+        }
+    }
+});
